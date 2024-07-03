e8036cb8b72590fe1cbb9347d95978e71d33bb74
Scott Hraban <scotthraban@gmail.com>
2024-07-03 15:16:35 -0700

    bump version to 1.2

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@ docker buildx build \
   --build-arg UID=1049 \
   --platform linux/arm64/v8,linux/amd64 \
   --push \
-  --tag docker-registry.example.com/mopidy:latest \
-  --tag docker-registry.example.com/mopidy:1.0 \
+  --tag docker-registry.hraban.com/mopidy:latest \
+  --tag docker-registry.hraban.com/mopidy:1.2 \
   .
